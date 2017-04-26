c89db1d835e1e79a2bc5a7ada7f78171bd53ff15
lixindong <antiless@163.com>
2017-04-26 08:35:29 +0800

    Add TalkingData sdk.
    Add multi channel packager.
    
    Issue: #9
    
    Signed-off-by: lixindong <antiless@163.com>

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -15,3 +15,13 @@
 #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
 #   public *;
 #}
+-dontwarn com.tendcloud.tenddata.**
+-keep class com.tendcloud.** {*;}
+-keep public class com.tendcloud.tenddata.** { public protected *;}
+-keepclassmembers class com.tendcloud.tenddata.**{
+public void *(***);
+}
+-keep class com.talkingdata.sdk.TalkingDataSDK {public *;}
+-keep class com.apptalkingdata.** {*;}
+-keep class dice.** {*; }
+-dontwarn dice.**
---
+++ app/src/main/java/com/sctdroid/app/textemoji/TextEmojiApplication.java
@@ -0,0 +1,18 @@
+package com.sctdroid.app.textemoji;
+
+import android.app.Application;
+
+import com.tendcloud.tenddata.TCAgent;
+
+/**
+ * Created by lixindong on 2017/4/26.
+ */
+public class TextEmojiApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        TCAgent.init(this);
+        // 如果已经在AndroidManifest.xml配置了App ID和渠道ID，调用TCAgent.init(this)即可；或与AndroidManifest.xml中的对应参数保持一致。
+        TCAgent.setReportUncaughtExceptions(true);
+    }
+}
