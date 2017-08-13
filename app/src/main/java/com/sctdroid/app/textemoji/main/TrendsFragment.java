e8663facb0fcfb795792131a86fcb770e7eebe9f
antiless <antiless@163.com>
2017-08-14 00:41:16 +0800

    Add VasSonic for webview.
    Signed-off-by: antiless <antiless@163.com>

--- app/src/main/java/com/sctdroid/app/textemoji/main/TrendsFragment.java
+++ app/src/main/java/com/sctdroid/app/textemoji/main/TrendsFragment.java
@@ -2,16 +2,20 @@ package com.sctdroid.app.textemoji.main;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.webkit.WebView;
 
 import com.sctdroid.app.textemoji.R;
+import com.sctdroid.app.textemoji.main.vassonic.SonicEngineHelper;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
 
 /**
  * Created by lixindong on 2017/8/12.
  */
 
-@EFragment(R.layout.activity_main)
+@EFragment(R.layout.layout_webview)
 public class TrendsFragment extends Fragment {
     public static TrendsFragment newInstance() {
 
@@ -20,5 +24,14 @@ public class TrendsFragment extends Fragment {
         TrendsFragment fragment = new TrendsFragment_();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @ViewById(R.id.webview)
+    WebView mWebview;
+
+    @AfterViews
+    void init() {
+        SonicEngineHelper helper = new SonicEngineHelper(getContext());
+        helper.start(mWebview, "http://www.baidu.com", null);
     }
 }
---
+++ app/src/main/java/com/sctdroid/app/textemoji/main/vassonic/SonicEngineHelper.java
@@ -0,0 +1,110 @@
+package com.sctdroid.app.textemoji.main.vassonic;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.tencent.sonic.sdk.SonicConfig;
+import com.tencent.sonic.sdk.SonicEngine;
+import com.tencent.sonic.sdk.SonicSession;
+import com.tencent.sonic.sdk.SonicSessionConfig;
+
+/**
+ * Created by lixindong on 2017/8/13.
+ */
+
+public class SonicEngineHelper {
+    private SonicSession sonicSession;
+    protected final Context mContext;
+
+    public SonicEngineHelper(Context context) {
+        this.mContext = context;
+    }
+
+    public Context getContext() {
+        return mContext;
+    }
+
+    public void start(WebView webView, String url, Object jsInterface) {
+        // step 1: Initialize sonic engine if necessary, or maybe u can do this when application created
+        if (!SonicEngine.isGetInstanceAllowed()) {
+            SonicEngine.createInstance(new SonicRuntimeImpl(getContext().getApplicationContext()), new SonicConfig.Builder().build());
+        }
+
+        SonicSessionClientImpl sonicSessionClient = null;
+
+        // step 2: Create SonicSession
+        sonicSession = SonicEngine.getInstance().createSession(url,  new SonicSessionConfig.Builder().build());
+        if (null != sonicSession) {
+            sonicSession.bindClient(sonicSessionClient = new SonicSessionClientImpl());
+        } else {
+            // this only happen when a same sonic session is already running,
+            // u can comment following codes to feedback as a default mode.
+//            throw new UnknownError("create session fail!");
+        }
+
+        // step 3: BindWebView for sessionClient and bindClient for SonicSession
+        // in the real world, the init flow may cost a long time as startup
+        // runtime、init configs....
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                if (sonicSession != null) {
+                    sonicSession.getSessionClient().pageFinish(url);
+                }
+            }
+
+            @TargetApi(21)
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                return shouldInterceptRequest(view, request.getUrl().toString());
+            }
+
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+                if (sonicSession != null) {
+                    //step 6: Call sessionClient.requestResource when host allow the application
+                    // to return the local data .
+                    return (WebResourceResponse) sonicSession.getSessionClient().requestResource(url);
+                }
+                return null;
+            }
+        });
+
+        WebSettings webSettings = webView.getSettings();
+
+        // step 4: bind javascript
+        // note:if api level lower than 17(android 4.2), addJavascriptInterface has security
+        // issue, please use x5 or see https://developer.android.com/reference/android/webkit/
+        // WebView.html#addJavascriptInterface(java.lang.Object, java.lang.String)
+        webSettings.setJavaScriptEnabled(true);
+        webView.removeJavascriptInterface("searchBoxJavaBridge_");
+//        intent.putExtra(SonicJavaScriptInterface.PARAM_LOAD_URL_TIME, System.currentTimeMillis());
+//        webView.addJavascriptInterface(jsInterface, "sonic");
+
+        // init webview settings
+        webSettings.setAllowContentAccess(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setSavePassword(false);
+        webSettings.setSaveFormData(false);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+
+
+        // step 5: webview is ready now, just tell session client to bind
+        if (sonicSessionClient != null) {
+            sonicSessionClient.bindWebView(webView);
+            sonicSessionClient.clientReady();
+        } else { // default mode
+            webView.loadUrl(url);
+        }
+    }
+}
